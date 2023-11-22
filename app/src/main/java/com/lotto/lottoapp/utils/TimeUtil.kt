587f7b8a5188c8b8e9c90e8cc2a30fd3e7eef323
Muhsin Ertuğan <muhsin.ertugan@sisal.com>
2023-11-22 10:46:08 +0300

    Refactor time functions

---
+++ app/src/main/java/com/lotto/lottoapp/utils/TimeUtil.kt
@@ -0,0 +1,31 @@
+package com.lotto.lottoapp.utils
+
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class TimeUtil {
+    private companion object{
+        val inputFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        val outputFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    }
+    fun calculateRemainingTime(targetDate: String): String {
+        val targetInstant = Instant.parse(targetDate)
+        val currentInstant = Instant.now()
+        val remainingTime = Duration.between(currentInstant, targetInstant)
+
+        val days = remainingTime.toDays()
+        val hours = remainingTime.toHours() % 24
+        val minutes = remainingTime.toMinutes() % 60
+        //val seconds = remainingTime.seconds % 60
+
+        return "${days}d ${hours}h ${minutes}m"
+    }
+
+    fun convertDateFormat(dateString: String): String {
+
+        val localDateTime = LocalDateTime.parse(dateString, inputFormatter)
+        return localDateTime.format(outputFormatter)
+    }
+}
