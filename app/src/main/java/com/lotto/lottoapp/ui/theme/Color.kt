5bbfc76b38a3a27fb05536b19b99e3ae829691c2
Muhsin Ertuğan <muhsin.ertugan@sisal.com>
2023-12-01 11:37:44 +0300

    Add color

--- app/src/main/java/com/lotto/lottoapp/ui/theme/Color.kt
+++ app/src/main/java/com/lotto/lottoapp/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.lotto.lottoapp.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val CustomPurple = Color(204, 0, 255)
+val CustomPurpleV2 = Color(122, 0, 244)
 val CustomBlack= Color(30, 30,30)
 val CustomGray = Color(red = 79, green = 79, blue = 79)
 val CustomLightGray = Color(red = 233, green = 233, blue = 233)
