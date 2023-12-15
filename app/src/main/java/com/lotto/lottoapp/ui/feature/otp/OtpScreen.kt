2339da359d0514dc4b515c5f7a181c7edbb4bbed
Muhsin Ertuğan <muhsin.ertugan@sisal.com>
2023-12-15 03:28:00 +0300

    Fix ResendBtn issue

--- app/src/main/java/com/lotto/lottoapp/ui/feature/otp/OtpScreen.kt
+++ app/src/main/java/com/lotto/lottoapp/ui/feature/otp/OtpScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -40,19 +41,21 @@ import com.lotto.lottoapp.model.request.RegisterOtpRequest
 import com.lotto.lottoapp.model.request.RegisterRequest
 import com.lotto.lottoapp.ui.constants.Constants
 import com.lotto.lottoapp.ui.feature.otp.components.CustomCountDownTimer
+import com.lotto.lottoapp.ui.feature.otp.components.ResendBtn
 import com.lotto.lottoapp.ui.theme.CustomGray
 import com.lotto.lottoapp.ui.theme.CustomLightGray
 import com.lotto.lottoapp.ui.theme.CustomPurple
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun OtpScreen(
     navController: NavHostController,
     userLoginInput: LoginRequest,
-    userRegisterInput: RegisterRequest
+    userRegisterInput: RegisterRequest,
 ) {
 
-
+    val scope = rememberCoroutineScope()
     val otpScreenViewModel: OtpScreenViewModel = hiltViewModel()
 
     var otpValue by remember { mutableStateOf("") }
@@ -61,6 +64,32 @@ fun OtpScreen(
 
 
     val inputColor = if (isRunning) CustomPurple else CustomGray
+
+
+    fun handleOtpCall() {
+        if (userRegisterInput.cityId == null) {
+            otpScreenViewModel.postLoginOtp(
+                navController = navController,
+                loginOtpRequest = LoginOtpRequest(
+                    email = userLoginInput.email,
+                    otp = otpValue
+                )
+            )
+        } else {
+            otpScreenViewModel.postRegisterOtp(
+                navController = navController,
+                registerOtpRequest = RegisterOtpRequest(
+                    birthDate = userRegisterInput.birthDate,
+                    cityId = userRegisterInput.cityId,
+                    email = userRegisterInput.email,
+                    lastName = userRegisterInput.lastName,
+                    name = userRegisterInput.name,
+                    phoneNumber = userRegisterInput.phoneNumber,
+                    otp = otpValue
+                )
+            )
+        }
+    }
 
     LaunchedEffect(isRunning) {
         if (isRunning) {
@@ -125,50 +154,32 @@ fun OtpScreen(
         )
     }
 
-    Button(
-        onClick = {
-            if (userRegisterInput.cityId == null) {
-                otpScreenViewModel.postLoginOtp(
-                    navController = navController,
-                    loginOtpRequest = LoginOtpRequest(
-                        email = userLoginInput.email,
-                        otp = otpValue
-                    )
-                )
+    if (isRunning) {
+        Button(
+            onClick = {
+                scope.launch {
+                    handleOtpCall()
+                }
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            enabled = isRunning,
+            modifier = Modifier
+                .width(96.dp)
+                .height(96.dp)
+        ) {
 
-            } else {
-                otpScreenViewModel.postRegisterOtp(
-                    navController = navController,
-                    registerOtpRequest = RegisterOtpRequest(
-                        birthDate = userRegisterInput.birthDate,
-                        cityId = userRegisterInput.cityId,
-                        email = userRegisterInput.email,
-                        lastName = userRegisterInput.lastName,
-                        name = userRegisterInput.name,
-                        phoneNumber = userRegisterInput.phoneNumber,
-                        otp = otpValue
-                    )
-                )
+            Image(
+                painter = painterResource(id = R.drawable.tickcircle),
+                contentDescription = "confirm button",
+                modifier = Modifier.scale(1.75F)
+            )
 
-            }
+        }
 
-        },
-        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-        enabled = isRunning,
-        modifier = Modifier
-            .width(96.dp)
-            .height(96.dp)
-    ) {
 
-        Image(
-            painter = painterResource(id = R.drawable.tickcircle),
-            contentDescription = "confirm button",
-            modifier = Modifier.scale(1.75F)
+        CustomCountDownTimer(
+            totalDuration = Constants.TOTAL_DURATION,
+            remainingTime = remainingTime
         )
-
-    }
-
-
-    CustomCountDownTimer(totalDuration = Constants.TOTAL_DURATION, remainingTime = remainingTime)
-
+    } else ResendBtn(handleResend = { handleOtpCall() })
 }
